c5792d0f5d81f075301f8082b6a25e8d0a76a613
Moutwei <moutweileechap@gmail.com>
2019-10-21 13:50:16 -0400

    Changed Link Address
    In config, https was changed to http.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://polar-savannah-03316.herokuapp.com',
+  production: 'http://polar-savannah-03316.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
